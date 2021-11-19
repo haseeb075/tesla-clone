fb83a515bbf62043a3dc058fbdcc9de37d537b86
haseeb075 <haseebiqbal000@gmail.com>
2021-11-19 23:19:35 +0500

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+    return (
+        <Container>
+            header
+        </Container>
+    )
+}
+ q
+export default Header
+
+const Container = styled.div`
+    
+`
