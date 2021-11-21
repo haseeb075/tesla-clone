f8fecdcdf8ee30f36872c544d1d5249d96908649
haseeb075 <haseebiqbal000@gmail.com>
2021-11-22 01:58:28 +0500

    adding header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,152 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, {useState} from "react";
+import styled from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from '@mui/icons-material/Close';
 
 function Header() {
-    return (
-        <Container>
-            header
-        </Container>
-    )
+    const [burgerStatus, setBurgerStatus] = useState(false);
+  return (
+    <Container>
+      <a>
+        <img src="/images/logo.svg" />
+      </a>
+
+      <Menu>
+        <a href="#">Model S</a>
+
+        <a href="#">Model 3</a>
+
+        <a href="#">Model X</a>
+
+        <a href="#">Model Y</a>
+      </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+        {/* hamsburger menu icon on right side */}
+        <CustomMenu onClick={() => setBurgerStatus(true)} />
+      </RightMenu>
+      <BurgerNav show={burgerStatus}>
+        {/* <ul> */}
+        <CloseWrapper>
+
+        <CustomClose onClick={()=> setBurgerStatus(false)} />
+        </CloseWrapper>
+          <li>
+            {" "}
+            <a href="#">Existing Inventory</a>
+          </li>
+          <li>
+            {" "}
+            <a href="#">Used Inventory</a>
+          </li>
+          <li>
+            {" "}
+            <a href="#">Trade-in</a>
+          </li>
+          <li>
+            {" "}
+            <a href="#">Cybertruck</a>
+          </li>
+          <li>
+            {" "}
+            <a href="#">Roadster</a>
+          </li>
+          <li>
+            {" "}
+            <a href="#">Existing Inventory</a>
+          </li>
+          <li>
+            {" "}
+            <a href="#">Existing Inventory</a>
+          </li>
+          <li>
+            {" "}
+            <a href="#">Existing Inventory</a>
+          </li>
+        {/* </ul> */}
+      </BurgerNav>
+    </Container>
+  );
 }
- q
-export default Header
+
+export default Header;
 
 const Container = styled.div`
-    
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 10px;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+`;
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+    flex-wrap: nowrap;
+  }
+`;
+const CustomMenu = styled(MenuIcon)`
+
+  cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  z-index: 16;
+  width:300px;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+//   burgerStatus ki value false aa rahi ha to translate nai kare ga else translate 100% kare ga
+  transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+  transition: transform 0.2s;
+  li{
+      padding: 15px 0;
+      border-bottom: 1px solid rgba(0,0,0,.2);
+
+      a{
+          font-weight: 600
+      }
+  }
+`;
+
+const CustomClose = styled(CloseIcon)`
+ cursor: pointer;
+`
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end; 
 `
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
 function Section(props) {
   console.log(props);
@@ -7,18 +8,21 @@ function Section(props) {
   const { title, description, backgroundImg, leftBtnText, rightBtnText } =
     props;
   return (
-    <Wrap bgImage = {backgroundImg}>
-      <ItemText>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </ItemText>
+    <Wrap bgImage={backgroundImg}>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
       <Buttons>
-        <ButtonGroup>
-          <LeftButton>{leftBtnText}</LeftButton>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
 
-          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
-          
-        </ButtonGroup>
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          </ButtonGroup>
+        </Fade>
 
         <DownArrow src="/images/down-arrow.svg" />
       </Buttons>
@@ -39,7 +43,7 @@ const Wrap = styled.div`
   flex-direction: column; //displays div or items in columns
   justify-content: space-between; //change the alignment of of flex itemns on main axis vertical
   align-items: center; //change the alignment of flex items horizontal
-  background-image: ${props => `url("images/${props.bgImage}")`}
+  background-image: ${(props) => `url("images/${props.bgImage}")`};
 `;
 
 const ItemText = styled.div`
